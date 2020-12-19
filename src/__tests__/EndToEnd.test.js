f61cc5f3488f1603b8f23558472700f0baff439d
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-12-19 14:48:37 +0100

    final debugging EndToEnd.test.js exercise 4.5

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -7,17 +7,17 @@ describe('show/hide an event details', () => {
     beforeAll(async () => {
         jest.setTimeout(50000);
         browser = await puppeteer.launch({
-            headless: false,
-            slowMo: 250, // slow down by 250ms
-            ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
+            headless: false
+            //slowMo: 250, // slow down by 250ms
+            //ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
         });
         page = await browser.newPage();
         await page.goto('http://localhost:3000/');
         await page.waitForSelector('.event');
     });
 
-    afterAll(() => {
-        browser.close();
+    afterAll(async () => {
+        await browser.close();
     });
 
     test('An event element is collapsed by default', async () => {
